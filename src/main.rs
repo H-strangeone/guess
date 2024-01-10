664c7460d2f6c4a944f2a824641f80d8674415f9
hitesh <hiteshgitman@gamil.com>
2024-01-10 10:57:41 -0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::io;
 use rand::Rng;
+use std::io;
 
 fn main() {
-    let random_number=rand::thread_rng();
-    println!("Hello, world!");
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    println!("please input your no");
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).expect("couldnt get it");
+    println!("you guessed {}", number);
+    println!("the number was {}", random_number);
 }

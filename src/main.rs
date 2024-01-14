7a6970d2c511c8664499a1c4e69ce2c2636e7aed
H-strangeone <hiteshgitman@gmail.com>
2024-01-13 23:48:12 -0800

    i

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1..=100);
+    let number = rand::thread_rng().gen_range(1..=100);
     println!("please input your no");
-    let mut number = String::new();
-    io::stdin().read_line(&mut number).expect("couldnt get it");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("couldnt get it");
+    let random_number= number.to_string();
     match guess.cmp(&random_number){
-        
+       Ordering::Less=> println!("naah fam"),
+       Ordering::Equal=> println!("yessir"),
+       Ordering::Greater=> println!("still naah fam"),
     }
-    println!("you guessed {}", number);
+    println!("you guessed {}", guess);
     println!("the number was {}", random_number);
 }
